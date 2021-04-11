0a00bef7d3015eab3202977a461a1980c0d13e6c
pe3-stack <pe3.gavrila@gmail.com>
2021-04-11 15:21:50 +0200

    last change

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,9 +9,9 @@ const navigationItems = ({ clicked }) => {
   return (
     <ul className='navigationItems'>
       <div>
-        <NavigationItem clicked={clicked} link='/playground-test/#weare'>contact us</NavigationItem>
+        <NavigationItem clicked={clicked} link='/playground-test/#contactus'>contact us</NavigationItem>
         <NavigationItem clicked={clicked} link='/playground-test/#careers'>careers</NavigationItem>
-        <NavigationItem clicked={clicked} link='/playground-test/#contactus'>we are</NavigationItem>
+        <NavigationItem clicked={clicked} link='/playground-test/#weare'>we are</NavigationItem>
         <NavigationItem clicked={clicked} link='/playground-test/#wedo'>we do</NavigationItem>
       </div>
     </ul>
